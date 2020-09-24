884946868b3cb0aec6afce38dc3a77201bd00945
kongfanyu <19388483@qq.com>
2020-09-24 14:44:17 +0800

    初次提交

---
+++ src/main/java/com/bank/MyProgram.java
@@ -0,0 +1,17 @@
+package com.bank;
+
+import java.util.Scanner;
+
+/**
+ * Copyright (C), 2018-2020
+ * FileName: MyProgram
+ * Author:   kongfanyu
+ * Date:     2020/9/24 14:32
+ */
+public class MyProgram {
+    public static void main(String[] args) {
+        System.out.println("恭喜你打开程序....按任意键退出..");
+        Scanner input = new Scanner(System.in);
+        input.next();
+    }
+}
